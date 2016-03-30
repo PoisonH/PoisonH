0ba3ff7978cd2d660e508811c3c03f967fb185db
PoisonH <PoisonH@163.com>
2016-03-31 07:36:50 +0800

    完善UI

--- app/src/main/java/com/poisonh/poisonh/MainActivity.java
+++ app/src/main/java/com/poisonh/poisonh/MainActivity.java
@@ -2,11 +2,17 @@ package com.poisonh.poisonh;
 
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.poisonh.poisonh.fragment.BluetoothFragment;
+import com.poisonh.poisonh.fragment.LocationFragment;
+import com.poisonh.poisonh.fragment.MusicFragment;
+import com.poisonh.poisonh.fragment.NewsFragment;
+import com.poisonh.poisonh.fragment.VedioFragment;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
 
@@ -14,28 +20,38 @@ public class MainActivity extends AppCompatActivity
 {
     private BottomBar mBottomBar;
 
+    private FragmentManager mFragmentManager = null;
+    private Fragment mNewsFragment;
+    private Fragment mBluetoothFragment;
+    private Fragment mMusicFragment;
+    private Fragment mVedioFragment;
+    private Fragment mLocationFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+        setTabSelection(0);
+
         mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener()
         {
             @Override
             public void onMenuTabSelected(@IdRes int menuItemId)
             {
+                setTabSelection(menuItemId);
             }
 
             @Override
             public void onMenuTabReSelected(@IdRes int menuItemId)
             {
-                Toast.makeText(getApplicationContext(), getMessage(menuItemId, true), Toast.LENGTH_SHORT).show();
             }
         });
 
-        // Setting colors for different tabs when there's more than three of them.
-        // You can set colors for tabs in three different ways as shown below.
+        // Setting colors for different tabs when there's more than three of them.You can set colors for tabs in three different ways as shown below.
         mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
         mBottomBar.mapColorForTab(1, 0xFF5D4037);
         mBottomBar.mapColorForTab(2, "#7B1FA2");
@@ -43,45 +59,120 @@ public class MainActivity extends AppCompatActivity
         mBottomBar.mapColorForTab(4, "#FF9800");
     }
 
-    private String getMessage(int menuItemId, boolean isReselection)
-    {
-        String message = "Content for ";
-
-        switch (menuItemId)
-        {
-            case R.id.bb_menu_recents:
-                message += "recents";
-                break;
-            case R.id.bb_menu_favorites:
-                message += "favorites";
-                break;
-            case R.id.bb_menu_nearby:
-                message += "nearby";
-                break;
-            case R.id.bb_menu_friends:
-                message += "friends";
-                break;
-            case R.id.bb_menu_food:
-                message += "food";
-                break;
-        }
-
-        if (isReselection)
-        {
-            message += " WAS RESELECTED! YAY!";
-        }
-
-        return message;
-    }
-
     @Override
     protected void onSaveInstanceState(Bundle outState)
     {
         super.onSaveInstanceState(outState);
-
-        // Necessary to restore the BottomBar's state, otherwise we would
-        // lose the current tab on orientation change.
+        // Necessary to restore the BottomBar's state, otherwise we would lose the current tab on orientation change.
         mBottomBar.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 根据传入的index参数来设置选中的tab页。
+     *
+     * @param index 0:首页，1：编程开发，2：程序员人生，3：快乐码农，4：开源软件
+     */
+    private void setTabSelection(int index)
+    {
+        // 开启一个Fragment事务
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        // 先隐藏掉所有的Fragment，以防止有多个Fragment显示在界面上的情况
+        hideFragments(transaction);
+        switch (index)
+        {
+            case 0:
+                if (mNewsFragment == null)
+                {
+                    // 如果mHomeFragment为空，则创建一个并添加到界面上
+                    mNewsFragment = new NewsFragment();
+                    transaction.add(R.id.fl_content, mNewsFragment);
+                } else
+                {
+                    // 如果mHomeFragment不为空，则直接将它显示出来
+                    transaction.show(mNewsFragment);
+                }
+                break;
+            case 1:
+                if (mBluetoothFragment == null)
+                {
+                    // 如果mProgrammerFragment为空，则创建一个并添加到界面上
+                    mBluetoothFragment = new BluetoothFragment();
+                    transaction.add(R.id.fl_content, mBluetoothFragment);
+                } else
+                {
+                    // 如果mProgrammerFragment不为空，则直接将它显示出来
+                    transaction.show(mBluetoothFragment);
+                }
+                break;
+            case 2:
+                if (mMusicFragment == null)
+                {
+                    // 如果mProgrammerLifeFragment为空，则创建一个并添加到界面上
+                    mMusicFragment = new MusicFragment();
+                    transaction.add(R.id.fl_content, mMusicFragment);
+                } else
+                {
+                    // 如果mProgrammerLifeFragment不为空，则直接将它显示出来
+                    transaction.show(mMusicFragment);
+                }
+                break;
+            case 3:
+                if (mVedioFragment == null)
+                {
+                    // 如果mWeeklyFragment为空，则创建一个并添加到界面上
+                    mVedioFragment = new VedioFragment();
+                    transaction.add(R.id.fl_content, mVedioFragment);
+                } else
+                {
+                    // 如果mWeeklyFragment不为空，则直接将它显示出来
+                    transaction.show(mVedioFragment);
+                }
+                break;
+            case 4:
+                if (mLocationFragment == null)
+                {
+                    // 如果mOpenSoftwareFragment为空，则创建一个并添加到界面上
+                    mLocationFragment = new LocationFragment();
+                    transaction.add(R.id.fl_content, mLocationFragment);
+                } else
+                {
+                    // 如果mOpenSoftwareFragment不为空，则直接将它显示出来
+                    transaction.show(mLocationFragment);
+                }
+                break;
+            default:
+                break;
+        }
+        transaction.commit();
+    }
+
+    /**
+     * 将所有的Fragment都置为隐藏状态。
+     *
+     * @param transaction 用于对Fragment执行操作的事务
+     */
+    private void hideFragments(FragmentTransaction transaction)
+    {
+        if (mNewsFragment != null)
+        {
+            transaction.hide(mNewsFragment);
+        }
+        if (mBluetoothFragment != null)
+        {
+            transaction.hide(mBluetoothFragment);
+        }
+        if (mMusicFragment != null)
+        {
+            transaction.hide(mMusicFragment);
+        }
+        if (mVedioFragment != null)
+        {
+            transaction.hide(mVedioFragment);
+        }
+        if (mLocationFragment != null)
+        {
+            transaction.hide(mLocationFragment);
+        }
     }
 }
 
---
+++ app/src/main/java/com/poisonh/poisonh/fragment/VedioFragment.java
@@ -0,0 +1,21 @@
+package com.poisonh.poisonh.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.poisonh.poisonh.R;
+
+/**
+ * Created by Administrator on 2016/3/30.
+ */
+public class VedioFragment extends Fragment
+{
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.layout_fragment_common, null);
+        return view;
+    }
+}
